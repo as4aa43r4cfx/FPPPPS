3484e7649f65227d658f5e17e8e9ebae39f6786e
as4aa43r4cfx <148439620+as4aa43r4cfx@users.noreply.github.com>
2024-11-14 18:50:05 +0900

    Update PlayerMove.cs

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -44,6 +44,8 @@ public class PlayerMove : MonoBehaviour
     // 애니메이터 변수
     Animator anim;
 
+    private Rigidbody myRigid;
+
     void Start()
     {
         // 캐릭터 콘트롤러 컴포넌트 받아오기
@@ -51,6 +53,9 @@ public class PlayerMove : MonoBehaviour
 
         // 애니메이터 받아오기
         anim = GetComponentInChildren<Animator>();
+
+        //리지드바디
+        myRigid = GetComponent<Rigidbody>();
     }
 
     void Update()
@@ -64,20 +69,23 @@ public class PlayerMove : MonoBehaviour
 
         // 키보드 <W>, <A>, <S>, <D> 버튼을 입력하면 캐릭터를 그 방향으로 이동시키고 싶다.
         // 키보드 <Space> 버튼을 입력하면 캐릭터를 수직으로 점프시키고 싶다.
+        public void Move()
+        {
+            // 1. 사용자의 입력을 받는다.
+            float h = Input.GetAxis("Horizontal");
+            float v = Input.GetAxis("Vertical");
 
-        // 1. 사용자의 입력을 받는다.
-        float h = Input.GetAxis("Horizontal");
-        float v = Input.GetAxis("Vertical");
+            // 2. 이동 방향을 설정한다.
+            Vector3 dir = new Vector3(h, 0, v);
+            dir = dir.normalized;
 
-        // 2. 이동 방향을 설정한다.
-        Vector3 dir = new Vector3(h, 0, v);
-        dir = dir.normalized;
+            // 이동 블랜딩 트리를 호출하고 벡터의 크기 값을 넘겨준다.
+            anim.SetFloat("MoveMotion", dir.magnitude);
 
-        // 이동 블랜딩 트리를 호출하고 벡터의 크기 값을 넘겨준다.
-        anim.SetFloat("MoveMotion", dir.magnitude);
-
-        // 2-1. 메인 카메라를 기준으로 방향을 변환한다.
-        dir = Camera.main.transform.TransformDirection(dir);
+            // 2-1. 메인 카메라를 기준으로 방향을 변환한다.
+            dir = Camera.main.transform.TransformDirection(dir);
+        }
+       
 
         // 2-2. 만일, 점프 중이었고, 다시 바닥에 착지했다면...
         if (isJumping && cc.collisionFlags == CollisionFlags.Below)
@@ -94,6 +102,7 @@ public class PlayerMove : MonoBehaviour
             // 캐릭터 수직 속도에 점프력을 적용하고 점프 상태로 변경한다.
             yVelocity = jumpPower;
             isJumping = true;
+            Jump();
         }
 
         // 2-4. 캐릭터 수직 속도에 중력 값을 적용한다.
@@ -101,18 +110,41 @@ public class PlayerMove : MonoBehaviour
         dir.y = yVelocity;
 
         // 3. 이동 속도에 맞춰 이동한다.
-        cc.Move(dir * moveSpeed * Time.deltaTime);
+        myRigid.Move(dir * moveSpeed * Time.deltaTime);
 
         // 4. 현재 플레이어 hp(%)를 hp 슬라이더의 value에 반영한다.
         //hpSlider.value = (float)hp / (float)maxHp;
 
     }
+    private void Jump()
+    {
+      
+    }
 
-    
 
-  
+    private void WaterCheck()
+    {
+        if (GameManager.isWater) // 물 속 일 때
+        {
+            if (Input.GetKeyDown(KeyCode.LeftShift)) // Shift 키 누르면 swimFastSpeed 속도로.
+                moveSpeed = swimFastSpeed;
+            else // 아니라면 swimSpeed 속도로.
+                moveSpeed = swimSpeed;
+        }
+    }
 
-    
+    private void TryJump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && !GameManager.isWater == true)
+            Jump();
+        else if (Input.GetKey(KeyCode.Space) && GameManager.isWater)
+            UpSwim(); // 스페이스 키를 꾹 누르는 중이고 물 속이라면 
+    }
+    private void UpSwim()
+    {
+        myRigid.velocity = transform.up * upSwimSpeed;
+    }
+
 
     // 플레이어의 피격 함수
     public void DamageAction(int damage)
