ac10d4a1d85a43476e2bc71c8bc096873f75100f
as4aa43r4cfx <148439620+as4aa43r4cfx@users.noreply.github.com>
2025-03-27 18:49:16 +0900

    asda
    asdad

--- Assets/Scripts/waterswitch.cs
+++ Assets/Scripts/waterswitch.cs
@@ -1,18 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class waterswitch : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private GameObject Switch;
+    [SerializeField] private GameObject Water1;
+    private float dist;
+    [SerializeField]
+    private Text actionText;
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        dist = Vector3.Distance(Switch.transform.position, transform.position);
+        if (dist < 4)
+        {
+            actionText.gameObject.SetActive(true);
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                Vector3 temp = Water1.transform.position;
+                temp.y += 1;
+                Water1.transform.position = temp;
+            }
+        }
+        else
+        {
+            actionText.gameObject.SetActive(false);
+        }
+
     }
 }
